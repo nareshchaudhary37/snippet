883b73fa68c234eaba17712f48fb2ede0a8153fb
Naresh chaudhary <nareshchaudhary37@gmail.com>
2018-05-30 09:55:16 +0530

    Fixed: handle doeble digits in  chronology number

--- chronology/algo.py
+++ chronology/algo.py
@@ -6,21 +6,22 @@ class ChronologyNumber(object):
     ORD_VALUE = 64
 
     def __char_to_number(self, string_number):
-        number = ""
+        number = []
         for char in list(string_number):
             num = ord(char) - self.ORD_VALUE
             if num == 26:
                 break
-            number += str(num)
+            number.append(num)
         return number
 
     def __number_to_char(self, number):
         string_number = ""
-        for num in list(number):
-            string_number += chr(int(num) + self.ORD_VALUE)
+        for num in number:
+            string_number += chr(num + self.ORD_VALUE)
         return string_number
 
     def get_next_number(self, current_string_number):
-        new_num = str(int(self.__char_to_number(current_string_number)) + 1)
+        new_num = self.__char_to_number(current_string_number)
+        new_num[-1] += 1
         missing = self.MAX_LENGTH_NUMBER - len(new_num)
-        return self.__number_to_char(new_num + "1"*missing)
+        return self.__number_to_char(new_num) + "A"*missing
